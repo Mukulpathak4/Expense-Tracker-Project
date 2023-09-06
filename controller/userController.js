84aa18359ab8fe293a3529fcfb6ff3e23ba5d074
Mukul pathak <mukulpathak4@gmail.com>
2023-09-06 18:04:29 +0530

    encryption done for password using bcrypt

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const User = require("../models/userModel");
-
+const bcrypt = require("bcrypt");
 
 const getSignUpPage = async (req, res, next) => {
   try {
@@ -10,27 +10,38 @@ const getSignUpPage = async (req, res, next) => {
   }
 };
 
-const postUserSignUp = async (req,res,next)=>{
-    try {
-      
-      const name = req.body.name;
+const postUserSignUp = async (req, res, next) => {
+  try {
+    const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
-      
-  
-      if (!email) {
-          throw new Error('email is mandatory !')
-      }
-      const data = await User.create({
-          name : name,
-          email : email,
-          password : password
+
+    await User.findOne({ where: { email: email } })
+      .then((user) => {
+        if (user) {
+          res
+            .status(409)
+            .send(
+              `<script>alert('This email is already taken. Please choose another one.'); window.location.href='/'</script>`
+            );
+        } else {
+          bcrypt.hash(password, 10, async (err, hash) => {
+            await User.create({
+              name: name,
+              email: email,
+              password: hash,
+            });
+          });
+          res
+            .status(200)
+            .send(
+              `<script>alert('User Created Successfully!'); window.location.href='/'</script>`
+            );
+        }
       })
-  
-      res.status(201).json({ newUserDetail: data });
-  } catch (err) {
-      console.log(err);
-      res.status(500).json({ error: err });
+      .catch((err) => console.log(err));
+  } catch (error) {
+    console.log(error);
   }
 }
 
@@ -48,21 +59,33 @@ const postUserLogin = async (req, res, next) => {
     const password = req.body.password;
 
     await User.findOne({ where: { email: email } }).then((user) => {
-      if (user.password != password)  {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, result) => {
+          if (err) {
+            return res
+              .status(500)
+              .json({ success: false, message: "Something went Wrong!" });
+          }
+          if (result == true) {
+            return res.status(200).json({
+              success: true,
+              message: "Login Successful!",
+            });
+          } else {
             return res.status(401).json({
               success: false,
               message: "Password Incorrect!",
             });
           }
-          else {
-            return res.status(200).json({
-              success: true,
-              message: "Login Successful!",
-            });
-          }
         });
-      } 
-  catch (error) {
+      } else {
+        return res.status(404).json({
+          success: false,
+          message: "User doesn't Exists!",
+        });
+      }
+    });
+  } catch (error) {
     console.log(error);
   }
 };
