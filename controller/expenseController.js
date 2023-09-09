76fdfdc1cae03565437e820bf7f42278b2288b16
Mukul pathak <mukulpathak4@gmail.com>
2023-09-09 21:01:42 +0530

    async await and  transaction also used

--- controller/expenseController.js
+++ controller/expenseController.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const Expense = require("../models/expenseModel");
-const database = require("../util/database");
 const User = require("../models/userModel");
+const sequelize = require("../util/database");
 
 // Controller function to render the home page
 exports.getHomePage = (req, res, next) => {
@@ -10,28 +10,47 @@ exports.getHomePage = (req, res, next) => {
 
 // Controller function to add an expense
 exports.addExpense = async (req, res, next) => {
-  try {
-    const { date, category, description, amount } = req.body;
+  const { date, category, description, amount } = req.body;
+  let t; // Declare the transaction variable
 
+  try {
+    t = await sequelize.transaction(); // Create a new transaction
+
+    // Increment totalExpenses for the user
     await User.increment('totalExpenses', {
       by: amount,
       where: { id: req.user.id },
+      transaction: t,
     });
 
+    // Create a new expense record
     await Expense.create({
       date: date,
       category: category,
       description: description,
       amount: amount,
       userId: req.user.id,
+    }, {
+      transaction: t,
     });
+
+    // Commit the transaction if all operations succeed
+    await t.commit();
 
     res.redirect("/homePage");
   } catch (err) {
     console.error(err);
+
+    if (t) {
+      // Rollback the transaction if an error occurs
+      await t.rollback();
+    }
+
     res.status(500).send("An error occurred while adding the expense.");
   }
 };
+
+
 
 // Controller function to get all expenses for the current user
 exports.getAllExpenses = async (req, res, next) => {
@@ -46,25 +65,45 @@ exports.getAllExpenses = async (req, res, next) => {
 
 // Controller function to delete an expense
 exports.deleteExpense = async (req, res, next) => {
+  let t; // Declare the transaction variable
+
   try {
     const id = req.params.id;
     const expense = await Expense.findByPk(id);
 
+    t = await sequelize.transaction(); // Create a new transaction
+
+    // Decrement totalExpenses for the user
     await User.decrement('totalExpenses', {
       by: expense.amount,
       where: { id: req.user.id },
+      transaction: t,
     });
 
-    await Expense.destroy({ where: { id: id, userId: req.user.id } });
+    // Delete the expense
+    await Expense.destroy({ where: { id: id, userId: req.user.id }, transaction: t });
+
+    // Commit the transaction if all operations succeed
+    await t.commit();
+
     res.redirect("/homePage");
   } catch (err) {
     console.error(err);
+
+    if (t) {
+      // Rollback the transaction if an error occurs
+      await t.rollback();
+    }
+
     res.status(500).send("An error occurred while deleting the expense.");
   }
 };
 
+
 // Controller function to edit an expense
 exports.editExpense = async (req, res, next) => {
+  let t; // Declare the transaction variable
+
   try {
     const id = req.params.id;
     const { category, description, amount } = req.body;
@@ -72,23 +111,38 @@ exports.editExpense = async (req, res, next) => {
     const expense = await Expense.findByPk(id);
     const amountDifference = amount - expense.amount;
 
+    t = await sequelize.transaction(); // Create a new transaction
+
+    // Increment or decrement totalExpenses for the user based on the amount difference
     await User.increment('totalExpenses', {
       by: amountDifference,
       where: { id: req.user.id },
+      transaction: t,
     });
 
+    // Update the expense
     await Expense.update(
       {
         category: category,
         description: description,
         amount: amount,
       },
-      { where: { id: id, userId: req.user.id } }
+      { where: { id: id, userId: req.user.id }, transaction: t }
     );
+
+    // Commit the transaction if all operations succeed
+    await t.commit();
 
     res.redirect("/homePage");
   } catch (err) {
     console.error(err);
+
+    if (t) {
+      // Rollback the transaction if an error occurs
+      await t.rollback();
+    }
+
     res.status(500).send("An error occurred while editing the expense.");
   }
 };
+
